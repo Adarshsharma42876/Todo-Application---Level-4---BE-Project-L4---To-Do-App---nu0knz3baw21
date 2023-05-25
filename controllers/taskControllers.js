f7d1ebe4cefb2c578f93c362156368b9a33804c9
ADARSH SHARMA <120009672+Adarshsharma42876@users.noreply.github.com>
2023-05-25 11:20:12 +0530

    Update taskControllers.js

--- controllers/taskControllers.js
+++ controllers/taskControllers.js
@@ -1,283 +1,164 @@
-const Users   = require("../models/user.js");
-
+const Users = require("../models/user.js");
 const jwt = require("jsonwebtoken");
-
-const Tasks   = require("../models/task.js");
-
-const bcrypt  = require('bcrypt');
-
+const Tasks = require("../models/task.js");
+const bcrypt = require("bcrypt");
 const { valid } = require("joi");
-
 const JWT_SECRET = "newtonSchool";
 
-const createTask =async (req, res) => {
+const createTask = async (req, res) => {
+  //creator_id is user id who have created this task.
 
-    //creator_id is user id who have created this task.
+  const { heading, description, token } = req.body;
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: "Invalid token",
+    });
+  }
+  const creator_id = decodedToken.userId;
 
-    const { heading, description, token  } = req.body;
+  const newtask = {
+    heading,
+    description,
+    creator_id,
+  };
 
-    let decodedToken;
-
-    try{
-
-        decodedToken = jwt.verify(token, JWT_SECRET);
-
-    }catch(err){
-
-        res.status(404).json({
-
-            status: 'fail',
-
-            message: 'Invalid token'
-
-        });
-
-    }
-
-    const creator_id = decodedToken.userId;
-
-    const newtask = {
-
-        heading,
-
-        description,
-
-        creator_id
-
-    };
-
-    try{
-
-        const task = await Tasks.create(newtask);
-
-        res.status(200).json({
-
-            message: 'Task added successfully',
-
-            task_id: task._id,
-
-            status: 'success'
-
-        });
-
-    }catch(error){
-
-        res.status(404).json({
-
-            status: 'fail',
-
-            message: error.message
-
-        });
-
-    };
-
-}
+  try {
+    const task = await Tasks.create(newtask);
+    res.status(200).json({
+      message: "Task added successfully",
+      task_id: task._id,
+      status: "success",
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
 
 const getdetailTask = async (req, res) => {
+  const task_id = req.body.task_id;
 
-    const task_id = req.body.task_id;
-
-    try{
-
-        const task = await Tasks.findById(task_id);
-
-        res.status(200).json({
-
-            status: 'success',
-
-            data: task
-
-        })
-
-    }catch(err){
-
-        res.status(404).json({
-
-            status: 'fail',
-
-            message: err.message
-
-        })
-
-    }
-
-}
+  try {
+    const task = await Tasks.findById(task_id);
+    res.status(200).json({
+      status: "success",
+      data: task,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
 
 const updateTask = async (req, res) => {
-
-    
-
-    const task_id = req.body.task_id;
-
-    try{
-
-        const task = await Tasks.findByIdAndUpdate(
-
-            task_id,
-
-            { $set:  req.body },
-
-            { new: true }
-
-        );
-
-        res.status(200).json({
-
-            status:'success',
-
-            data: task
-
-        });
-
-    }catch(err){
-
-        res.status(404).json({
-
-            status:'fail',
-
-            data: err.message
-
-        });
-
-    }
-
-}
+  const task_id = req.body.task_id;
+  try {
+    const task = await Tasks.findByIdAndUpdate(
+      task_id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json({
+      status: "success",
+      data: task,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      data: err.message,
+    });
+  }
+};
 
 const deleteTask = async (req, res) => {
+  const { task_id, token } = req.body;
 
-    const {task_id, token} = req.body;
-
-    try{
-
-        await Tasks.findByIdAndDelete(task_id);
-
-        res.status(200).json({
-
-            status: 'success',
-
-            message: 'Task deleted successfully'
-
-        });
-
-    }catch(err){
-
-        res.status(404).json({
-
-            status: 'fail',
-
-            message: err.message
-
-        })
-
-    };
-
-}
+  try {
+    await Tasks.findByIdAndDelete(task_id);
+    res.status(200).json({
+      status: "success",
+      message: "Task deleted successfully",
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
 
 /*
 
 req.body = {
-
     token: token
-
 }
 
 can be given status in query.
-
 /api/v1/task/?status=pending
-
 /api/v1/task/
 
 if status is given filter only document contaning Status value as query status.
 
 if token belongs to admin than show task of all user.
-
 if token belongs to any other user than show task belong to that user only.
-
 Payload Structure is given in loginUser controller.
 
 response:
 
 200 status Code
-
 json = {
-
         status:'success',
-
         data: [//contaning all task object].
-
     }
 
 and sort the return data in opposite order of creation date.
-
 the latest data will be at the top.
 
 */
 
 const getallTask = async (req, res) => {
-
-    //Write your code here.
-
-    
-
-    const { token  } = req.body;
-
-    let decodedToken;
-
-    try{
-
-        decodedToken = jwt.verify(token, JWT_SECRET);
-
-    }catch(err){
-
-        res.status(404).json({
-
-            status: 'fail',
-
-            message: 'Invalid token'
-
-        });
-
+  //Write your code here.
+  try {
+    const {token} = req.body;
+    if (!token) {
+        return res.status(401).json({ status: 'fail', message: "Authentication failed: Missing token." });
     }
-
-    const creator_id = decodedToken.userId;
-
-    
-
-    const user= await Users.findById({creator_id})
-
-    
-
-    if(user.role==='admin'){
-
-        const alltask= await Tasks.find({}).sort({timestamps: -1})
-
-        
-
-        res.status(200).json({status:'success', data: [alltask]})
-
-    }else{
-
-        const task= await Tasks.find({creator_id:creator_id}).sort({timestamps: -1})
-
-        res.status(200).json({status:'success', data: [task]})
-
+    let { status } = req.query;
+    // const { userId } = jwt.verify(token, JWT_SECRET);
+    if (!status) {
+        status = "pending"
     }
+    // const user = await Users.findById(userId);
+    // if (user.role === "admin") {
+    //   const data = Tasks.find({ status: status });
+    //   return res.json({
+    //     status: "success",
+    //     data,
+    //   });
 
-    
+    // }
+    const data = await Tasks.find({ status: status, });
+    res.json({
+        status: "success",
+        data,
+      })
+  } catch (error) {
+    res.status(200).json({ message: 'Internal Server Error' , status: 'success', error: error.message })
+  }
+};
 
-    
-
-}
-
-module.exports = { createTask, getdetailTask, updateTask, deleteTask, getallTask };
-
-
-
-
-
-
-
-
-
-
+module.exports = {
+  createTask,
+  getdetailTask,
+  updateTask,
+  deleteTask,
+  getallTask,
+};
